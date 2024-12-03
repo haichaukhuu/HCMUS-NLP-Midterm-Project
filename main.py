46aaaa28cdd2d396773f361f32667b4713349bb6
Khưu Hải Châu <haichaukhuu@gmail.com>
2024-12-03 22:12:01 +0700

    main changed to suit file format changes

--- main.py
+++ main.py
@@ -5,19 +5,26 @@ from align_cn_vn_bboxes import *
 # sample data
 file = "Data/HaiChau_Luan Ngu C4.pdf" 
 text_dir = f'./extracted_text/'
-csv_file = "processed_data.csv"
+processed_data_json_file = "processed_data.json"
 
 if __name__ == "__main__":
+    # uncomment to extract text from pdf
+    # --------------------------------
+
     # extract_text_from_pdf(file, text_dir)
     
-    # delete files that dont contain chinese text
-    # df = process_data_files(text_dir) # 2 columns: text, bbox
+    # # delete files that dont contain chinese text
+    # processed_data = process_data_files(text_dir) # 2 columns: text, bbox
 
-    # Save the processed dataframe to CSV
-    # df.to_csv(csv_file, index=False)
-    # print(f"Saved processed data to {csv_file}")
+    # # Save the processed data to JSON
+    # with open(processed_data_json_file, 'w', encoding='utf-8') as f:
+    #     json.dump(processed_data, f, ensure_ascii=False, indent=4)
+    # print(f"Saved processed data to {processed_data_json_file}")
+    
+    # --------------------------------
+
 
     # align bboxes
     print("Aligning bboxes...")
-    aligned_df = align_cn_vn_bboxes(csv_file)
+    aligned_df = align_cn_vn_bboxes(processed_data_json_file)
     print(aligned_df)
